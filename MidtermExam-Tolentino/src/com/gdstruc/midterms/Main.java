0e1511fc5a603a4aef29be522b793c2ebc51fa3d
An Tolentino <an.tolentino@benilde.edu.ph>
2022-10-23 21:32:13 +0800

    MIDTERM EXAM-Tolentino

---
+++ MidtermExam-Tolentino/src/com/gdstruc/midterms/Main.java
@@ -0,0 +1,32 @@
+package com.gdstruc.midterms;
+
+import java.util.Stack;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Stack<Card> deck = new Stack<>();
+
+        // Push elements
+        for (int j = 0; j < 30; j++) {
+            deck.push(new Card(j+1));
+        }
+        System.out.println("Top of Player Deck: "+ deck.peek());
+
+        Stack<Card> discardedPile = new Stack<>();
+        Stack<Card> cardOnHand = new Stack<>();
+
+        // Initialize game and setting stacks
+        CardStack game = new CardStack();
+        game.setPlayerDeck(deck);
+        game.setDiscardedPile(discardedPile);
+        game.setCardOnHand(cardOnHand);
+
+        // Prints Player Deck with 30 cards
+        game.printPlayerDeck(deck);
+
+        game.gameOn();
+
+    }
+}
